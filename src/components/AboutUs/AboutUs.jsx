ac38dd652665954309415f7d384f9f6b4a64a8e0
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-03-02 15:40:07 +0100

    about us layout

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,7 +1,44 @@
+import './AboutUs.css'
+
 export default function AboutUs() {
   return (
-    <div className="about-us">
-      <h1>Abou tUs</h1>
-    </div>
+    <main className="about-us page">
+      <header className="about-us__header">
+        <h5>About Us</h5>
+        <div className="title about-us__title">
+          <h1>NFT ART</h1>
+        </div>
+      </header>
+      <section className="about-us__content">
+        <hr className="about-us__hr_first" />
+        <div className="about-us__description">
+          <p className="p1">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo.
+            <br />
+            <br />
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+            fugit, sed quia consequuntur magni dolores eos qui ratione
+            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+            ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non
+            numquam eius modi tempora incidunt ut labore et dolore magnam
+            aliquam quaerat voluptatem.
+          </p>
+        </div>
+        <div className="about-us__email email">
+          <h5>Email</h5>
+          <div className="email__link-content">
+            <a className="email__link" href="mailto: nft@art.com">
+              <h2>nft@art.com</h2>
+            </a>
+            <hr className="about-us__hr_second" />
+          </div>
+        </div>
+        <hr className="about-us__hr_third" />
+        <hr className="about-us__hr_forth" />
+      </section>
+    </main>
   )
 }
