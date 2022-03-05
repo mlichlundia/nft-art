a924cd675e3903c06598951e56101d85b6ef900d
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-03-05 13:19:18 +0100

    responsive and adaptive styles

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -12,13 +12,13 @@ export default function AboutUs() {
       <section className="about-us__content">
         <hr className="about-us__hr_first" />
         <div className="about-us__description">
-          <p className="p1">
+          <p className="p1 description__first-paragraph">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
             quae ab illo inventore veritatis et quasi architecto beatae vitae
             dicta sunt explicabo.
-            <br />
-            <br />
+          </p>
+          <p className="p1">
             Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
             fugit, sed quia consequuntur magni dolores eos qui ratione
             voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
@@ -26,14 +26,15 @@ export default function AboutUs() {
             numquam eius modi tempora incidunt ut labore et dolore magnam
             aliquam quaerat voluptatem.
           </p>
-        </div>
-        <div className="about-us__email email">
-          <h5>Email</h5>
-          <div className="email__link-content">
-            <a className="email__link" href="mailto: nft@art.com">
-              <h2>nft@art.com</h2>
-            </a>
-            <hr className="about-us__hr_second" />
+
+          <div className="about-us__email email">
+            <h5>Email</h5>
+            <div className="email__link-content">
+              <a className="email__link" href="mailto: nft@art.com">
+                <h2>nft@art.com</h2>
+              </a>
+              <hr className="about-us__hr_second" />
+            </div>
           </div>
         </div>
         <hr className="about-us__hr_third" />
--- src/components/MainPage/ver1/MainPage_ver1.jsx
+++ src/components/MainPage/ver1/MainPage_ver1.jsx
@@ -8,63 +8,67 @@ export default function MainPage() {
 
   return (
     <main className="main page">
-      <section className="main__art-container">
-        <div className="main_art-info art-info">
-          <div className="art-info__index-name">
-            <h5 className="art-info__index">01</h5>
-            <h5>Irene Francesca</h5>
+      <div className="main-container">
+        <section className="main__art-container">
+          <div className="main-art-info-container">
+            <div className="main_art-info art-info">
+              <div className="art-info__index-name">
+                <h5 className="art-info__index">01</h5>
+                <h5 className="art-info__name">Irene Francesca</h5>
+              </div>
+              <h5 className="art-info__year">2022</h5>
+              <h5 className="art-info__title">OK</h5>
+            </div>
+            <hr className="main__hr_first" />
           </div>
-          <h5 className="art-info__year">2022</h5>
-          <h5>OK</h5>
-        </div>
-        <hr className="main__hr_first" />
-        <div className="main__art">
           <img
+            className="main__art"
             src="https://images.unsplash.com/photo-1606318621597-c057f7d4926e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
             alt="art"
           />
-        </div>
-      </section>
-      <section className="title main__title">
-        <h1>OK</h1>
-      </section>
-      <section className="main__content">
-        <p className="p3">
-          Digital painting. Comes with Gallery Quality Print. Shipping &
-          packaging included. Print only guaranteed for the first buyer. Printed
-          with Canon Pro 1000 with original inks on 350gsm Hahnemühle Museum
-          Etching paper. Size 420x594mm. 20mm border. Signed and numbered
-          edition 1 of 1.
-        </p>
-        <div className="main__authorship">
-          <div className="main__avatar-container">
-            <img
-              className="avatar"
-              src="https://images.unsplash.com/photo-1516585427167-9f4af9627e6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-              alt="avatar"
-            />
-            <hr className="main__hr_second" />
-            <h3>Irene Francesca</h3>
-          </div>
-          <p className="p3">
+        </section>
+        <section className="main__content">
+          <section className="title main__title">
+            <h1>OK</h1>
+          </section>
+
+          <p className="p3 main__description">
             Digital painting. Comes with Gallery Quality Print. Shipping &
             packaging included. Print only guaranteed for the first buyer.
+            Printed with Canon Pro 1000 with original inks on 350gsm Hahnemühle
+            Museum Etching paper. Size 420x594mm. 20mm border. Signed and
+            numbered edition 1 of 1.
           </p>
-        </div>
-        <div className="collect">
-          <div className="collect__price-container">
-            <CurrencySelect
-              currency={currency}
-              setCurrency={setCurrency}
-              price={price}
-              setPrice={setPrice}
-            />
+          <div className="main__authorship">
+            <div className="main__avatar-container">
+              <img
+                className="avatar"
+                src="https://images.unsplash.com/photo-1516585427167-9f4af9627e6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                alt="avatar"
+              />
+              <hr className="main__hr_second" />
+              <h3>Irene Francesca</h3>
+            </div>
+            <p className="p3">
+              Digital painting. Comes with Gallery Quality Print. Shipping &
+              packaging included. Print only guaranteed for the first buyer.
+            </p>
           </div>
-          <a className="collect__button" href="https://">
-            Collect
-          </a>
-        </div>
-      </section>
+          <div className="collect">
+            <div className="collect__price-container">
+              <CurrencySelect
+                currency={currency}
+                setCurrency={setCurrency}
+                price={price}
+                setPrice={setPrice}
+              />
+            </div>
+            <a className="collect__button" href="https://">
+              Collect
+            </a>
+          </div>
+        </section>
+      </div>
     </main>
   )
 }
