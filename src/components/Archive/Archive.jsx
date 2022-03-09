3ed7b2374108ad4114e2cf5d9ead1ee73157a04f
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-03-09 17:47:06 +0100

    add pop up

--- src/components/Archive/Archive.jsx
+++ src/components/Archive/Archive.jsx
@@ -1,7 +1,13 @@
 import './Archive.css'
 import PinterestBoard from './gallery/PinterestBoard/PinterestBoard'
+import PopUp from '../PopUp/PopUp'
+import { useState } from 'react'
 
 export default function Archive() {
+  const [isActive, setIsActive] = useState(false)
+  const [image, setImage] = useState(
+    'https://images.unsplash.com/photo-1608284894748-9ec2239bdb14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  )
   return (
     <main className="archive page">
       <header className="archive__header">
@@ -11,7 +17,12 @@ export default function Archive() {
           <hr className="archive__hr" />
         </div>
       </header>
-      <PinterestBoard />
+      <PinterestBoard
+        isActive={isActive}
+        setIsActive={setIsActive}
+        setImage={setImage}
+      />
+      <PopUp image={image} isActive={isActive} setIsActive={setIsActive} />
     </main>
   )
 }
