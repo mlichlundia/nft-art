fbcb895479c593fa49556d8930185a3181ca76f4
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-03-09 12:28:02 +0100

    add open art animation

--- src/components/MainPage/ver1/MainPage_ver1.jsx
+++ src/components/MainPage/ver1/MainPage_ver1.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './MainPage_ver1.css'
 import CurrencySelect from '../CurrencySelect/CurrencySelect'
 
@@ -6,11 +6,148 @@ export default function MainPage() {
   const [currency, setCurrency] = useState('USD')
   const [price, setPrice] = useState('495')
 
+  const [artHeight, setArtHeight] = useState('')
+  const [artWidth, setArtWidth] = useState('')
+
+  const main = useRef()
+  const mainContent = useRef()
+  const image = useRef()
+  const info = useRef()
+  const closeArtBlock = useRef()
+
+  // sets start height and width, fixes size of image if it is more than window height
+  useEffect(() => {
+    const art = image.current
+    const content = mainContent.current
+    const mainHeight = window.innerHeight * 0.8
+    const firstArtHeight = art.clientHeight
+
+    console.log(firstArtHeight, art.clientWidth, 'first')
+    art.clientHeight > mainHeight
+      ? setArtHeight(mainHeight + 'px')
+      : setArtHeight(art.clientHeight + 'px')
+    art.style.height =
+      art.clientHeight > mainHeight
+        ? mainHeight + 'px'
+        : art.clientHeight + 'px'
+
+    console.log(mainHeight, firstArtHeight, 'height')
+
+    const scaleW = mainHeight / firstArtHeight
+    firstArtHeight > mainHeight
+      ? setArtWidth(scaleW * art.clientWidth + 'px')
+      : setArtWidth(art.clientWidth + 'px')
+    art.style.width =
+      firstArtHeight > mainHeight
+        ? art.clientWidth * scaleW + 'px'
+        : art.clientWidth + 'px'
+
+    console.log(art.style.height, artWidth, scaleW, 'after')
+
+    content.style.height = content.clientHeight + 'px'
+  }, [])
+
+  const [height, setHeight] = useState('')
+  const [windowWidth, setWindowWidth] = useState({})
+
+  function changeHeight() {
+    const mainContentHeight = mainContent.current.clientHeight
+    const imgHeight = image.current.clientHeight
+    if (windowWidth >= 992) {
+      mainContentHeight >= imgHeight
+        ? setHeight(`${mainContentHeight}px`)
+        : setHeight(`${imgHeight}px`)
+    } else {
+      setHeight('')
+    }
+  }
+
+  useEffect(() => {
+    setWindowWidth(document.documentElement.clientWidth)
+    changeHeight()
+  }, [windowWidth])
+
+  function openArt() {
+    if (windowWidth < 992) {
+      return
+    }
+
+    const mainWidth =
+      main.current.clientWidth -
+      parseInt(
+        getComputedStyle(main.current).getPropertyValue('padding-left'),
+      ) *
+        2
+    const art = image.current
+    const content = mainContent.current
+    const infoBlock = info.current
+
+    setTimeout(() => {
+      const scaleW = parseInt(height) / parseInt(art.style.height)
+      art.style.width =
+        art.clientWidth * scaleW > mainWidth
+          ? mainWidth + 'px'
+          : art.clientWidth * scaleW + 'px'
+
+      const scaleH = mainWidth / parseInt(artWidth) / scaleW
+      art.style.height =
+        art.clientWidth * scaleW > mainWidth
+          ? parseInt(height) * scaleH + 'px'
+          : height
+
+      art.style.transform =
+        art.clientWidth * scaleW > mainWidth
+          ? ''
+          : `translate(${
+              mainWidth / 2 - parseInt(image.current.style.width) / 2 + 'px'
+            })`
+      infoBlock.style.transform =
+        art.clientWidth * scaleW > mainWidth
+          ? ''
+          : `translate(${
+              mainWidth / 2 - parseInt(image.current.style.width) / 2 + 'px'
+            })`
+      console.log(mainWidth / 2 - parseInt(image.current.style.width) / 2)
+    }, 560)
+
+    content.className = content.className + ' main__content_leave'
+
+    closeArtBlock.current.style.visibility = 'visible'
+  }
+
+  function closeArt() {
+    if (windowWidth < 992) {
+      return
+    }
+
+    const art = image.current
+    const content = mainContent.current
+    const infoBlock = info.current
+    console.log(art.clientWidth, art.clientHeight)
+    art.style.width = artWidth
+    art.style.height = artHeight
+
+    setTimeout(() => {
+      content.className = content.className.slice(
+        0,
+        content.className.indexOf(' '),
+      )
+    }, 560)
+
+    art.style.transform = ''
+    infoBlock.style.transform = ''
+    closeArtBlock.current.style.visibility = ''
+  }
+
   return (
-    <main className="main page">
+    <main className="main page" ref={main}>
       <div className="main-container">
         <section className="main__art-container">
-          <div className="main-art-info-container">
+          <div
+            className="main-art-info-container"
+            style={{ height: `${height}` }}
+            ref={info}
+          >
             <div className="main_art-info art-info">
               <div className="art-info__index-name">
                 <h5 className="art-info__index">01</h5>
@@ -21,13 +158,23 @@ export default function MainPage() {
             </div>
             <hr className="main__hr_first" />
           </div>
+
           <img
             className="main__art"
             src="https://images.unsplash.com/photo-1606318621597-c057f7d4926e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+            // src="https://images.unsplash.com/photo-1628788704043-a595bf2f5cd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+            // src="https://images.unsplash.com/photo-1545050029-23994f940b2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=677&q=80"
             alt="art"
+            ref={image}
+            onClick={openArt}
           />
         </section>
-        <section className="main__content">
+        <div
+          className="main__close-art"
+          ref={closeArtBlock}
+          onClick={closeArt}
+        ></div>
+        <section className="main__content" ref={mainContent}>
           <section className="title main__title">
             <h1>OK</h1>
           </section>
