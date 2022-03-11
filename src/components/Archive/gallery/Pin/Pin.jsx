c453c1dd8a39b59f1e1583f27fd7bc57a96d96cc
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-03-11 21:32:28 +0100

    add container to images

--- src/components/Archive/gallery/Pin/Pin.jsx
+++ src/components/Archive/gallery/Pin/Pin.jsx
@@ -2,14 +2,16 @@ import './Pin.css'
 
 export default function Pin({ setIsActive, setImage, src, alt }) {
   return (
-    <img
-      className="image"
-      src={src}
-      alt={alt}
-      onClick={() => {
-        setIsActive(true)
-        setImage(src)
-      }}
-    ></img>
+    <div className="image-container">
+      <img
+        className="image"
+        src={src}
+        alt={alt}
+        onClick={() => {
+          setIsActive(true)
+          setImage(src)
+        }}
+      ></img>
+    </div>
   )
 }
