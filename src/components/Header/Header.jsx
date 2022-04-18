89f666e0155cd7c0c1d2e235b8df0cecbee0c373
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-04-18 15:43:15 +0200

    fix layout styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,25 @@
-import './Header.css'
-import { NavLink } from 'react-router-dom'
+import "./Header.css"
+import { NavLink } from "react-router-dom"
 
 export default function Header() {
-  const menuList = [
-    { title: 'Archive', path: 'archive' },
-    { title: 'About Us', path: 'about_us' },
-  ]
-  return (
-    <nav>
-      <div className="logo menu__point">
-        <NavLink to="" key="main">
-          <p className="p2">Main</p>
-        </NavLink>
-      </div>
-      <ul className="menu">
-        {menuList.map((point) => (
-          <NavLink className="menu__point" to={point.path} key={point.path}>
-            <p className="p2">{point.title}</p>
-          </NavLink>
-        ))}
-      </ul>
-    </nav>
-  )
+	const menuList = [
+		{ title: "Archive", path: "archive" },
+		{ title: "About Us", path: "about_us" },
+	]
+	return (
+		<nav>
+			<div className='menu__point'>
+				<NavLink to='' key='main'>
+					<p className='p2'>Main</p>
+				</NavLink>
+			</div>
+			<ul className='menu'>
+				{menuList.map(point => (
+					<NavLink className='menu__point' to={point.path} key={point.path}>
+						<p className='p2'>{point.title}</p>
+					</NavLink>
+				))}
+			</ul>
+		</nav>
+	)
 }
